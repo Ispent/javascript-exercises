431641488803dc14c815309be81e544d6b555cba
Patty Lee <seunghak2003@gmail.com>
2024-01-12 17:06:20 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+  if (typeof val1 !== 'number' || typeof val2 !== 'number') return 'ERROR'
+  if (val1 < 0 || val2 < 0) return 'ERROR'
+  
+  let sum = (val1 > val2) ? val1 : val2;
+  for (let i = 0; i <= (Math.abs(val1 - val2)); i++) {
+    sum += i;
+  }
+  return sum;
+  
 };
 
 // Do not edit below this line
