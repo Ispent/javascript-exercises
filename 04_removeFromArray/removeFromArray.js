534e3420626a08dc9757909009812626dc31a806
Patty Lee <seunghak2003@gmail.com>
2024-01-12 16:36:39 -0500

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,16 @@
 const removeFromArray = function(numArray, ...numbers) {
   // takes the array, and counts through every value and checks if the number in the second argument is in the array
   // need to chek argument count, so use argument.legnth != 2 to cancel out
-  let args = [...numbers];
-  return args;
-  for (let i = 0; i < numArray.length(); i++) {
-    if (delNum == numArray[i]) {
-      delete numArray[i];
+
+  for (let i = 0; i < numArray.length; i++) {
+    for (let j = 0; j < numbers.length; j++){
+    if (numArray[i] === numbers[j]) {
+      numArray.splice(i, 1)
+      i--;
+    }
     }
   }
+  return numArray;
 };
 
 // Do not edit below this line
